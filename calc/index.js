c4c53a5ba51fb444d852fb41ac35ca87f27a932e
Almazn1k <hayday5589@gmail.com>
2025-08-28 10:25:09 +0300

    Added trigonometry, exponents and roots

--- calc/index.js
+++ calc/index.js
@@ -2,14 +2,15 @@ const firstInput = document.getElementById('firstInput');
 const secondInput = document.getElementById('secondInput');
 const button = document.querySelector('.equals');
 const select = document.getElementById('select');
+const ms = document.getElementById('ms')
 const container = document.querySelector('.container');
 const resultDisp = document.getElementById('result');
 const copy = document.querySelector('.copy');
 
 function showResult(result) {
-    if (result == "Infinity") {
+    if (result == "Infinity" || String(result) == "NaN") {
         resultDisp.style.color = "red";
-        resultDisp.textContent = "Ерор";
+        resultDisp.textContent = "Ty dolbaeb";
         
     } else {
         resultDisp.style.color = "black";
@@ -29,6 +30,10 @@ function calc() {
     const firstValue = Number(firstInput.value);
     const secondValue = Number(secondInput.value);
     const operation = select.value;
+    const metricsys = ms.value;
+
+    console.log(firstValue);
+    console.log(secondValue);
 
     let result = 0;
 
@@ -44,6 +49,42 @@ function calc() {
             break;
         case "divide":
             showResult(firstValue / secondValue);
+            break;
+        case "sqrt":
+            showResult(Math.pow(firstValue, 0.5));
+            break;
+        case "sqr":
+            showResult(Math.pow(firstValue, 2));
+            break;
+        case "exponent":
+            showResult(Math.pow(firstValue, secondValue));
+            break;
+        case "nrt":
+            showResult(Math.pow(secondValue, 1/firstValue));
+            break;
+        case "sin":
+            if (String(metricsys) == "deg"){
+                showResult(Math.sin(firstValue * Math.PI / 180));
+            }
+            else if (String(metricsys) == "rad"){
+                showResult(Math.sin(Math.PI / firstValue));
+            }
+            break;
+        case "cos":
+            if (String(metricsys) == "deg"){
+                showResult(Math.cos(firstValue * Math.PI / 180));
+            }
+            else if (String(metricsys) == "rad"){
+                showResult(Math.cos(Math.PI / firstValue));
+            }
+            break;
+        case "tg":
+            if (String(metricsys) == "deg"){
+                showResult(Math.tan(firstValue * Math.PI / 180));
+            }
+            else if (String(metricsys) == "rad"){
+                showResult(Math.tan(Math.PI / firstValue));
+            }
             break;
         default:
             return;
